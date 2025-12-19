f5fe1edbb83f8b738f3616c1eee4376bc396c3f8
Andres <andresrojo20013@gmail.com>
2025-12-19 12:09:50 -0500

    se crea controlador de envio de correo #2

---
+++ backend/src/contact/controllers/contact.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ContactService } from '../services/contact.service';
+import { CreateContactDto } from '../dto/create-contact.dto';
+
+@Controller('contact')
+export class ContactController {
+  constructor(private readonly contactService: ContactService) {}
+
+  @Post()
+  async send(@Body() body: CreateContactDto) {
+    await this.contactService.sendContactEmail(
+      body.name,
+      body.lastName,
+      body.email,
+      body.category,
+      body.musicPlatform,
+      body.yourSocials,
+      body.message,
+    );
+
+    return { message: 'Mensaje enviado correctamente' };
+  }
+}
