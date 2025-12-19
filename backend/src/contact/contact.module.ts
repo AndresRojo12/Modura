a1b95925a754a3b59b679891f4abc7619d1b9637
Andres <andresrojo20013@gmail.com>
2025-12-19 12:48:53 -0500

    se configura modulo de contact #4

--- backend/src/contact/contact.module.ts
+++ backend/src/contact/contact.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ContactService } from './services/contact.service';
+import { ContactController } from './controllers/contact.controller';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+    imports: [ConfigModule],
+    controllers: [ContactController],
+    providers: [ContactService],
+})
 export class ContactModule {}
